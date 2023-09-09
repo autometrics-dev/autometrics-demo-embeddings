f94ffd542e45ed1ab575be83a38f79ab6e7fa282
Brett Beutell <brbeut@gmail.com>
2023-09-09 15:56:46 +0200

    Add autometrics to python app

--- embeddings/app.py
+++ embeddings/app.py
@@ -1,12 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from transformers import AutoTokenizer, AutoModel
+from autometrics import autometrics
+from prometheus_client import generate_latest
 
 app = Flask(__name__)
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 model = AutoModel.from_pretrained("bert-base-uncased")
 
+# Set up a metrics endpoint for Prometheus to scrape
+# `generate_latest` returns the latest metrics data in the Prometheus text format
+@app.route("/metrics", methods=["GET"])
+def metrics():
+    return Response(generate_latest())
+
+
 @app.route("/embeddings", methods=["POST"])
+@autometrics
 def embeddings():
     data = request.json
     text = data["text"]
